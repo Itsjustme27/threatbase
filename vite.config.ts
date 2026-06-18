3d36fdbfae09d27dd810da5438fe167d4a6e9cd8
kalidada18 <kalidada18@users.noreply.github.com>
2026-06-18 14:03:59 +0545

    fix: update vite base URL to '/' to resolve broken images

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.CF_PAGES ? '/' : '/threatbase/',
+  base: '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
